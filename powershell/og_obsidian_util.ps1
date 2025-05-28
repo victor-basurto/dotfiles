37ea9669c1789585c2d9ae34413f63be81effd36
Victor Basurto <my.mvplace@gmail.com>
2025-05-28 01:17:19 -0700

    todo obsidian tag note creation

--- powershell/og_obsidian_util.ps1
+++ powershell/og_obsidian_util.ps1
@@ -4,4 +4,7 @@ $ZETTELKASTEN_DIR = Join-Path $VAULT_PATH "zettelkasten"
 $NOTES_DIR = Join-Path $vaultPath "notes"
 
 # iterate through all the markdown files in the zettelkasten directory
-
+# TODO: read each of the files in the zettelkasten directory and extract the tags
+# if a tag is found, move the file to the nodes/<tag> directory
+# if the tag directory does not exist, create it
+# if there is no tag, ignore and return
