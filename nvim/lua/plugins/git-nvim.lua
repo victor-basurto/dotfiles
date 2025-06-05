11f9345e0f4e232f893f783815ac59d6465b864e
Victor Basurto <my.mvplace@gmail.com>
2025-06-04 22:33:39 -0700

    keymaps for git

--- nvim/lua/plugins/git-nvim.lua
+++ nvim/lua/plugins/git-nvim.lua
@@ -5,9 +5,11 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup({})
+      require("gitsigns").setup({ current_line_blame = true })
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview hunk" })
       vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Preview blame" })
+      vim.keymap.set("n", "<leader>gF", ":Gitsigns blame_line<CR>", { desc = "Blame line" })
+      vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis<CR>", { desc = "diff" })
     end,
   },
 }
