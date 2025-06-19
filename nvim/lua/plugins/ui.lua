bf0542e079e1b19c95950bcb19989a332c568341
Victor Basurto <my.mvplace@gmail.com>
2025-06-19 11:54:57 -0700

    moved snacks.explorer to right side

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -9,4 +9,27 @@ return {
     end,
   },
   { "bullets-vim/bullets.vim" },
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      picker = {
+        -- NOTE: documentation for `snacks.explorer`
+        --https://github.com/folke/snacks.nvim/blob/main/docs/picker.md#%EF%B8%8F-layouts
+        sources = {
+          explorer = {
+            layout = {
+              preset = "right",
+              layout = {
+                height = 0,
+                min_width = 50,
+                -- position = "right",
+                border = "none",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
