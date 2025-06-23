4e8542466a9e5bfc8bb042be3064b3ac2a8ba40e
Victor Basurto <my.mvplace@gmail.com>
2025-06-23 10:24:27 -0700

    snippets for jekyll and prettierignore helpers

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,57 +1,5 @@
 return {
   { "smjonas/inc-rename.nvim", opts = {} },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    ---@class CustomCmpConfig : cmp.ConfigSchema
-    ---@param opts CustomCmpConfig
-    opts = function(_, opts)
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-      opts.snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      }
-
-      -- add sources
-      table.insert(opts.sources, { name = "emoji" })
-      table.insert(opts.sources, { name = "luasnip" })
-      -- Configure Tab behavior
-      opts.mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-      })
-    end,
-    -- stylua: ignore
-    keys = {
-      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
-      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
-    }
-,
-  },
   { "bullets-vim/bullets.vim" },
   {
     "folke/snacks.nvim",
