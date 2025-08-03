68386792670c023677d82b6c8adbff07547b1cdb
Victor Basurto <my.mvplace@gmail.com>
2025-08-02 20:08:22 -0700

    initialize todo-comments

--- nvim/lua/plugins/ts-comments.lua
+++ nvim/lua/plugins/ts-comments.lua
@@ -1,6 +1,26 @@
 return {
-  "folke/ts-comments.nvim",
-  opts = {},
-  event = "VeryLazy",
-  enabled = vim.fn.has("nvim-0.10.0") == 1,
+  { "folke/ts-comments.nvim", opts = {}, event = "VeryLazy", enabled = vim.fn.has("nvim-0.10.0") == 1 },
+  {
+    "folke/todo-comments.nvim",
+    optional = true,
+    enabled = false,
+    keys = {
+      {
+        -- {
+        --   "<leader>ct",
+        --   function()
+        --     Snacks.picker.todo_comments()
+        --   end,
+        --   desc = "Todo",
+        -- },
+        {
+          "<leader>cT",
+          function()
+            Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+          end,
+          desc = "Todo/Fix/Fixme",
+        },
+      },
+    },
+  },
 }
