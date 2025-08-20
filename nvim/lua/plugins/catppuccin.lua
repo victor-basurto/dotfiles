bc5891011b2f2cdf4c80ce28631605a443cd4207
Victor Basurto <my.mvplace@gmail.com>
2025-08-20 09:54:14 -0700

    docs: add todo for bufferline using catppuccin

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -20,6 +20,7 @@ return {
     optional = true,
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
+    -- TODO: add buffer line configuration
     -- opts = function(_, opts)
     --   if (vim.g.colors_name or ""):find("catppuccin") then
     --     opts.highlights = require("catppuccin.groups.integrations.bufferline").get({})
