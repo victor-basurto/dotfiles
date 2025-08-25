02aeb3796a94cdaa5646a65cea3fc991dd3cc09e
Victor Basurto <my.mvplace@gmail.com>
2025-08-25 14:20:15 -0700

    catppuccin order

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,31 +1,48 @@
 return {
   {
     "catppuccin/nvim",
-    lazy = true,
+    lazy = false,
     name = "catppuccin",
     priority = 1000,
-    opts = {
-      transparent_background = true,
-      flavour = "frappe",
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+    config = function()
+      local catppuccin = require("catppuccin")
+      catppuccin.setup({
+        transparent_background = true,
+        float = {
+          transparent = false,
+          solid = false,
+        },
+        flavour = "macchiato",
+        integrations = {
+          bufferline = {
+            enabled = true,
+            highlights = true, -- get highlights for bufferline
+            style = "default", -- "default" | "minimal" (optional)
+          },
+        },
+      })
+
+      vim.cmd("colorscheme catppuccin")
+    end,
   },
   {
     "akinsho/bufferline.nvim",
     optional = true,
-    version = "*",
-    priority = 1000,
-    dependencies = "nvim-tree/nvim-web-devicons",
-    -- TODO: add buffer line configuration
-    -- opts = function(_, opts)
-    --   if (vim.g.colors_name or ""):find("catppuccin") then
-    --     opts.highlights = require("catppuccin.groups.integrations.bufferline").get({})
-    --   end
-    -- end,
+    opts = function(_, opts)
+      local ok, buf = pcall(require, "catppuccin.groups.integrations.bufferline")
+      if ok and buf.get_theme then
+        local palettes = require("catppuccin.palettes")
+        local mocha = palettes.get_palette("mocha")
+        opts.highlights = buf.get_theme({
+          styles = { "italic", "bold" },
+          custom = {
+            all = { fill = { bg = "#000000" } },
+            mocha = { background = { fg = mocha.text } },
+            latte = { background = { fg = "#000000" } },
+          },
+        })
+      end
+      return opts
+    end,
   },
 }
