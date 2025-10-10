c64616f944999f2883f876faff12c8daa04f0c3f
Victor Basurto <my.mvplace@gmail.com>
2025-10-10 16:09:22 -0700

    feat: update lsp for typescriptreact

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,17 +1,35 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          -- null_ls.builtins.formatting.prettier,
+          require("none-ls.diagnostics.eslint"),
+        },
+      })
+      -- vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
   },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.formatting.prettier,
-        require("none-ls.diagnostics.eslint"),
-      },
-    })
-    -- vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+  {
+    "mfusseneger/nvim-lint",
+    config = function()
+      require("lint").linters_by_ft = {
+        javascript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescript = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+      }
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "InsertLeave" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
+  },
 }
