393a3303ff72b983a74363122b0f950e912a75a7
Victor Basurto <my.mvplace@gmail.com>
2026-03-29 12:12:32 -0700

    fix: remove fzf reference and switch to snacks

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
-vim.g.lazyvim_picker = "fzf"
+vim.g.lazyvim_picker = "snacks"
 -- vim.g.lazyvim_prettier_needs_config = true
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
