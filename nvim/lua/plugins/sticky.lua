834a0a188802506de7ea142a2a04359238c3ebfd
Victor Basurto <my.mvplace@gmail.com>
2026-03-30 00:23:55 -0700

    feat: add fusen and notes plugin for notes

--- nvim/lua/plugins/sticky.lua
+++ nvim/lua/plugins/sticky.lua
@@ -5,7 +5,7 @@ return {
     event = "VimEnter",
     opts = {
       -- file location where the annotations are going to be saved
-      save_file = vim.fn.expand("$HOME") .. "/.config/fusen/notes/notes.json",
+      save_file = vim.fn.expand("$HOME") .. "/.config/sticky-notes/notes/notes.json",
       -- Disable all default keymaps (so "me", "mc", etc. won't conflict)
       keymaps = {
         add_mark = "me",
@@ -33,13 +33,56 @@ return {
       -- keymap mappings
       local fusen = require("fusen")
 
-      vim.keymap.set("n", "<leader>Ne", fusen.add_mark, { desc = "[Notes]: Add/Edit note" })
-      vim.keymap.set("n", "<leader>Nc", fusen.clear_mark, { desc = "[Notes]: Clear note" })
-      vim.keymap.set("n", "<leader>NC", fusen.clear_buffer, { desc = "[Notes]: Clear buffer notes" })
-      vim.keymap.set("n", "<leader>ND", fusen.clear_all, { desc = "[Notes]: Clear ALL notes" })
-      vim.keymap.set("n", "<leader>Nn", fusen.next_mark, { desc = "[Notes]: Next note" })
-      vim.keymap.set("n", "<leader>Np", fusen.prev_mark, { desc = "[Notes]: Previous note" })
-      vim.keymap.set("n", "<leader>Nl", fusen.list_marks, { desc = "[Notes]: List all notes" })
+      -- [N]otes [a]nnotattion [e]dit/add
+      vim.keymap.set("n", "<leader>Nae", fusen.add_mark, { desc = "[Annotation]: Add/Edit note" })
+      -- [N]otes [a]nnotattion [c]lear note
+      vim.keymap.set("n", "<leader>Nac", fusen.clear_mark, { desc = "[Annotation]: Clear note" })
+      -- [N]otes [a]nnotattion [C]lear buffer notes
+      vim.keymap.set("n", "<leader>NaC", fusen.clear_buffer, { desc = "[Annotation]: Clear buffer notes" })
+      -- [N]otes [a]nnotattion [D] Clear All Notes
+      vim.keymap.set("n", "<leader>NaD", fusen.clear_all, { desc = "[Annotation]: Clear ALL notes" })
+      -- [N]otes [a]nnotattion [n]ext note
+      vim.keymap.set("n", "<leader>Nan", fusen.next_mark, { desc = "[Annotation]: Next note" })
+      -- [N]otes [a]nnotattion [p]revious note
+      vim.keymap.set("n", "<leader>Nap", fusen.prev_mark, { desc = "[Annotation]: Previous note" })
+      -- [N]otes [a]nnotattion [l]ist all notes
+      vim.keymap.set("n", "<leader>Nal", fusen.list_marks, { desc = "[Annotation]: List all notes" })
+    end,
+  },
+  {
+    "etiennecollin/notes.nvim",
+    lazy = false,
+    cmd = { "NotesToggle", "NotesShow", "NotesHide", "NotesEdit" },
+    opts = {
+      notes_file_path = vim.fn.expand("$HOME") .. "/.config/sticky-notes/notes/all-notes.md",
+      auto_save = true,
+      auto_save_on_exit = true,
+      display_mode = "vsplit",
+      split = {
+        hsplit_height = 30,
+        vsplit_width = 30,
+      },
+      buffer_keymaps = {
+        save = "<C-s>",
+        quit = "qq",
+      },
+    },
+    config = function(_, opts)
+      require("notes").setup(opts)
+
+      -- Main toggle: Open / Close notes in vertical split on the right
+      vim.keymap.set("n", "<leader>Nnt", "<cmd>NotesToggle vsplit<CR>", { desc = "[Notes] Toggle right sidebar" })
+
+      -- Open as floating window (good for temporary use)
+      vim.keymap.set("n", "<leader>NnF", "<cmd>NotesToggle float<CR>", { desc = "[Notes] Toggle floating window" })
+
+      -- Force show / hide (useful if you want separate control)
+      vim.keymap.set("n", "<leader>Nns", "<cmd>NotesShow<CR>", { desc = "[Notes] Show notes" })
+
+      vim.keymap.set("n", "<leader>Nnh", "<cmd>NotesHide<CR>", { desc = "[Notes] Hide notes" })
+
+      -- Directly edit the notes file in current window (rarely needed)
+      vim.keymap.set("n", "<leader>Nne", "<cmd>NotesEdit<CR>", { desc = "[Notes] Edit notes file" })
     end,
   },
 }
