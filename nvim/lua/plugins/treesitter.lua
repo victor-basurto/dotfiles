e84d43fe1eed0f37f42929fa25f9a05d496d4b79
Victor Basurto <my.mvplace@gmail.com>
2026-04-20 22:39:58 -0700

    feat: add primeagen refactoring

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,76 +1,114 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  lazy = false,
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  main = "nvim-treesitter.configs",
-  opts = {
-    ensure_installed = {
-      "astro",
-      "bash",
-      "comment",
-      "css",
-      "git_config",
-      "git_rebase",
-      "gitattributes",
-      "gitcommit",
-      "gitignore",
-      "go",
-      "graphql",
-      "html",
-      "http",
-      "javascript",
-      "json",
-      "json5",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "powershell",
-      "prisma",
-      "regex",
-      "scss",
-      "sql",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "xml",
-      "yaml",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    ignore_install = {},
-    highlight = { enable = true },
-    indent = { enable = true },
-    auto_install = true,
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
+    main = "nvim-treesitter.configs",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "bash",
+        "comment",
+        "css",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "graphql",
+        "html",
+        "http",
+        "javascript",
+        "json",
+        "json5",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "powershell",
+        "prisma",
+        "regex",
+        "scss",
+        "sql",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+      ignore_install = {},
+      highlight = { enable = true },
+      indent = { enable = true },
+      auto_install = true,
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
         },
       },
+      sync_install = false,
     },
-    sync_install = false,
-  },
-  config = function(_, opts)
-    -- We don't call the require here because 'main' + 'opts' does it for us.
-    -- If you MUST call it manually, use pcall to prevent the hard crash:
-    local status, treesitter = pcall(require, "nvim-treesitter.configs")
-    if status then
-      treesitter.setup(opts)
-    end
+    config = function(_, opts)
+      -- We don't call the require here because 'main' + 'opts' does it for us.
+      -- If you MUST call it manually, use pcall to prevent the hard crash:
+      local status, treesitter = pcall(require, "nvim-treesitter.configs")
+      if status then
+        treesitter.setup(opts)
+      end
 
-    -- Your TSX Autocmd
-    -- vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-    --   pattern = "*.tsx",
-    --   callback = function()
-    --     vim.bo.filetype = "typescriptreact"
-    --   end,
-    -- })
-  end,
+      -- Your TSX Autocmd
+      -- vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+      --   pattern = "*.tsx",
+      --   callback = function()
+      --     vim.bo.filetype = "typescriptreact"
+      --   end,
+      -- })
+    end,
+  },
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "lewis6991/async.nvim",
+    },
+    config = function(_, opts)
+      local refactoring = require("refactoring")
+      refactoring.setup(opts)
+
+    -- stylua: ignore
+    vim.keymap.set("x", "<leader>re", function() return refactoring.extract_func() end,   { expr = true, desc = "Extract Function" })
+      vim.keymap.set("x", "<leader>rE", function()
+        return refactoring.extract_func_to_file()
+      end, { expr = true, desc = "Extract Function to File" })
+      vim.keymap.set("x", "<leader>rv", function()
+        return refactoring.extract_var()
+      end, { expr = true, desc = "Extract Variable" })
+      vim.keymap.set("n", "<leader>ri", function()
+        return refactoring.inline_var()
+      end, { expr = true, desc = "Inline Variable" })
+      vim.keymap.set("x", "<leader>ri", function()
+        return refactoring.inline_var()
+      end, { expr = true, desc = "Inline Variable" })
+      vim.keymap.set("n", "<leader>rI", function()
+        return refactoring.inline_func()
+      end, { expr = true, desc = "Inline Function" })
+      vim.keymap.set("n", "<leader>rs", function()
+        return refactoring.select_refactor()
+      end, { expr = true, desc = "Select Refactor" })
+      vim.keymap.set("x", "<leader>rs", function()
+        return refactoring.select_refactor()
+      end, { expr = true, desc = "Select Refactor" })
+    end,
+  },
 }
