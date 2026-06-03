b596b70412a70f72497fa61e52106916e180f722
Victor Basurto <my.mvplace@gmail.com>
2026-06-02 22:38:47 -0700

    fix: completion for nvim-cmp and blink is now povided by obsidian

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -108,17 +108,17 @@ return {
       template = "notes.md",
     },
 
-    completion = (function()
-      local has_nvim_cmp = pcall(require, "cmp")
-      local has_blink = pcall(require, "blink.cmp")
-      return {
-        nvim_cmp = has_nvim_cmp and not has_blink,
-        blink = has_blink,
-        min_chars = 2,
-        match_case = true,
-        create_new = true,
-      }
-    end)(),
+    -- completion = (function()
+    --   local has_nvim_cmp = pcall(require, "cmp")
+    --   local has_blink = pcall(require, "blink.cmp")
+    --   return {
+    --     nvim_cmp = has_nvim_cmp and not has_blink,
+    --     blink = has_blink,
+    --     min_chars = 2,
+    --     match_case = true,
+    --     create_new = true,
+    --   }
+    -- end)(),
 
     templates = {
       enabled = true,
