3ddb0df21d274c45c696496b1505b7490f59b447
Victor Basurto <my.mvplace@gmail.com>
2026-06-17 21:29:58 -0700

    fix: update luasnip with luarocks

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -34,6 +34,9 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+  rocks = {
+    enabled = true,
+  },
   -- install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
