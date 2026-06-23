cc9c6c1e315321dad98ceb580eedc19e307cf03d
Victor Basurto <my.mvplace@gmail.com>
2026-06-22 19:35:36 -0700

    feat: orginize source zsh files

---
+++ zsh/functions/source.zsh
@@ -0,0 +1,12 @@
+# directory utils
+source "$HOME/.config/zsh/functions/dir-utils.zsh"
+source "$HOME/.config/zsh/functions/utils.zsh"
+source "$HOME/.config/zsh/functions/eza-utils.zsh"
+
+# obsidian utils
+source "$HOME/.config/zsh/functions/og.zsh"
+source "$HOME/.config/zsh/functions/on.zsh"
+source "$HOME/.config/zsh/functions/odn.zsh"
+
+# cheatsheets
+source "$HOME/.config/zsh/functions/cheat.zsh"
