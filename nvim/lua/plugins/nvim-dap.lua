47bdd9c1b72620c8ebda495e29f724a15d3d5b40
Victor Basurto <my.mvplace@gmail.com>
2026-09-01 01:03:55 -0700

    feat: add nvim-dadbod database plugin

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -77,4 +77,55 @@ return {
       end
     end,
   },
+
+  -- database
+  {
+    "tpope/vim-dadbod",
+    cmd = "DB",
+  },
+  {
+    "kristijanhusak/vim-dadbod-completion",
+  },
+  {
+    "kristijanhusak/vim-dadbod-completion",
+    ft = { "sql", "mysql", "plsql" },
+    config = function()
+      require("blink.cmp").setup({
+        sources = {
+          default = { "lsp", "path", "buffer" },
+          providers = {
+            ["vim-dadbod-completion"] = {
+              module = "vim_dadbod_completion",
+              name = "vim-dadbod-completion",
+              score_offset = 100,
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    dependencies = "vim-dadbod",
+    keys = {
+      { "<leader>D", "<cmd>DBUIToggle<CR>", desc = "Toggle DBUI" },
+    },
+    init = function()
+      local data_path = vim.fn.stdpath("data")
+
+      vim.g.db_ui_auto_execute_table_helpers = 1
+      vim.g.db_ui_save_location = data_path .. "/dadbod_ui"
+      vim.g.db_ui_show_database_icon = true
+      vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
+      vim.g.db_ui_use_nerd_fonts = true
+      vim.g.db_ui_use_nvim_notify = true
+
+      -- NOTE: The default behavior of auto-execution of queries on save is disabled
+      -- this is useful when you have a big query that you don't want to run every time
+      -- you save the file running those queries can crash neovim to run use the
+      -- default keymap: <leader>S
+      vim.g.db_ui_execute_on_save = false
+    end,
+  },
 }
